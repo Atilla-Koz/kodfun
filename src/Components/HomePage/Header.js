5c98eca7830fd6d9787ed713911343034a4bdea2
Atilla-Koz <atillakoz@hotmail.com>
2024-08-21 13:41:40 +0300

    chore: Add Google Fonts and update font styles
    ```

--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="flex flex-row justify-between gap-8 p-8 bg-black text-white">
       <section>
-        <h1>A</h1>
+        <button>A</button>
       </section>
       <section className="flex flex-row justify-between gap-8">
         <button>About</button>
--- src/Components/HomePage/Hero.js
+++ src/Components/HomePage/Hero.js
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <div className="flex flex-col justify-center items-center gap-8 mt-8">
-      <h1>Not My First Webpage</h1>
-      <h4>Bootcamp "CodeFun is approaching.</h4>
+      <h1 className="font-eduVic">Not My First Webpage</h1>
+      <h4 className="font-eduVic">Bootcamp "CodeFun" is approaching.</h4>
     </div>
   );
 }
