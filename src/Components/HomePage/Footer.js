1661e09f962cc327322830657a84c2b202333a17
Atilla-Koz <atillakoz@hotmail.com>
2024-08-21 14:28:33 +0300

    chore: Add Footer component to HomePage

---
+++ src/Components/HomePage/Footer.js
@@ -0,0 +1,30 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-row justify-between gap-8 p-8 bg-black text-white w-full absolute bottom-0">
+      <a
+        href="https://github.com/Atilla-Koz"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {' '}
+        <button className="font-eduVic">GitHub</button>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/atilla-k%C3%B6z-9b1841194/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {' '}
+        <button className="font-eduVic">Linkedin</button>
+      </a>
+      <a
+        href="https://www.instagram.com/atillakz/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {' '}
+        <button className="font-eduVic">Instagram</button>
+      </a>
+    </div>
+  );
+}
--- src/Layout/HomePage.js
+++ src/Layout/HomePage.js
@@ -1,11 +1,14 @@
+import Footer from '../Components/HomePage/Footer';
 import Header from '../Components/HomePage/Header';
 import Hero from '../Components/HomePage/Hero';
 
 export default function HomePage() {
   return (
-    <div className=" w-full overflow-x-hidden">
+    <div className="flex flex-col min-h-screenz w-full overflow-x-hidden">
       <Header />
       <Hero />
+      <div className="flex-grow"></div>
+      <Footer />
     </div>
   );
 }
